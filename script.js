8c500a750b7b5ebb8aae034f309ca481e14399f9
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2022-08-22 09:22:36 +0300

    local storage, clear button, event lsitener on enter
    finished product, made local storage  to work, can remove items by icon, added event listener to windows on enter key

--- script.js
+++ script.js
@@ -1,9 +1,58 @@
-let input = document.querySelector("input");
-let list = document.querySelector(".list-container");
-let addButton = document.querySelector(".add");
-addButton.addEventListener("click", function () {
+const input = document.querySelector("input");
+const list = document.querySelector(".list-container");
+const addButton = document.querySelector(".add");
+const clear = document.querySelector(".clear");
+
+if (localStorage.getItem("tasks") !== null) {
+  storage = JSON.parse(localStorage.getItem("tasks"));
+  storage.forEach((item) => {
+    list.innerHTML =
+      list.innerHTML + `<li>${item}<i class="fa-solid fa-trash-can"></i></li>`;
+  });
+}
+let deleteBtns = document.querySelectorAll(".fa-trash-can");
+function remover() {
+  deleteBtns = document.querySelectorAll(".fa-trash-can");
+  deleteBtns.forEach((elem) => {
+    elem.addEventListener("click", function () {
+      let content = elem.parentElement.textContent;
+      let array = JSON.parse(localStorage.getItem("tasks"));
+      let index = array.indexOf(content);
+      let final = array.splice(index, 1);
+      console.log(array);
+      localStorage.setItem("tasks", JSON.stringify(array));
+
+      elem.parentElement.remove();
+      elem.remove();
+      deleteBtns = document.querySelectorAll(".fa-trash-can");
+    });
+  });
+}
+remover();
+function loader() {
   if (input.value !== "") {
-    list.innerHTML = list.innerHTML + `<li>${input.value}</li>`;
-    input.value = "";
+    list.innerHTML =
+      list.innerHTML +
+      `<li>${input.value} <i class="fa-solid fa-trash-can"></i></li>`;
+
+    if (localStorage.getItem("tasks") === null) {
+      localStorage.setItem("tasks", JSON.stringify([]));
+    }
+    let storage = JSON.parse(localStorage.getItem("tasks"));
+    storage.push(input.value);
+    localStorage.setItem("tasks", JSON.stringify(storage));
+    remover();
   }
+  input.value = "";
+}
+addButton.addEventListener("click", loader);
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    loader();
+  }
+});
+// make clear butn
+clear.addEventListener("click", () => {
+  localStorage.clear();
+  list.innerHTML = "";
 });
